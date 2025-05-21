1dcc4670ff3ccb23205dbe254bab8f6cd8d2d1dd
EGIJPCNG <1005.30075634.ucla@gmail.com>
2025-05-21 15:14:28 -0400

    Setting_Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Prov from './prov.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/prov" element={<Prov />} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/tasks" element={<h1>Tasks Page</h1>} />
+        <Route path="/add-task" element={<h1>New Task</h1>} />
+        <Route path="/task/:id" element={<h1>Update Task</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
---
+++ client/src/prov.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Prov() {
+    return (
+        <article>
+            <div>
+                <h1 className="rainbow-text text-4xl font-extrabold">Welcome to the React App</h1>
+                <h1 className="tex-4xl font-bold">Welcome to the React App</h1>
+                <h1 className="">Welcome to the React App</h1>
+                <h1>Welcome to the React App</h1>
+            </div>
+        </article>
+    );
+}
+
+export default Prov;
